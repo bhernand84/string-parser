d7952743760a9d1f34dcbe64fbcb657ce5cff0db
Benaldo Hernandez <bhernandez@tonicdesign.com>
2015-05-30 16:19:05 -0400

    add project solution and test library

---
+++ string-parser-tests/string-parser-tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace string_parser_tests
+{
+    [TestClass]
+    public class StringParseTests
+    {
+        [TestMethod]
+        public void GivenStringEmployeeObjectParsesCorrectly()
+        {
+            
+        }
+    }
+}
